3ea22d8dbd214f2decc9d2abe3989fd920df2cbd
Elaina Bialkowski <elainabialkowski@outlook.com>
2022-07-29 15:06:36 -0400

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/elainabialkowski/itermtohex
+
+go 1.18
+
+require howett.net/plist v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"howett.net/plist"
+)
+
+func main() {
+	filePath := os.Args[1]
+	itermcolors, err := os.Open(filePath)
+	if err != nil {
+		return
+	}
+	defer itermcolors.Close()
+	itermbytes, _ := io.ReadAll(itermcolors)
+
+	colors := make(map[string]map[string]interface{})
+	_, err = plist.Unmarshal(itermbytes, &colors)
+
+	for k, ansi := range colors {
+		var hex string
+		var red, green, blue int
+
+		for k, component := range ansi {
+			switch component := component.(type) {
+			case float64:
+				real := component
+				switch k {
+				case "Red Component":
+					red = int(real * 255)
+				case "Blue Component":
+					green = int(real * 255)
+				case "Green Component":
+					blue = int(real * 255)
+				}
+			}
+		}
+
+		hex = fmt.Sprintf("#%x%x%x", red, green, blue)
+		fmt.Printf("%s: %s\n", k, hex)
+	}
+
+}
